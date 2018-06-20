6ffc312637059c4b0cdd52d7030f79efeb6fdf90
ted.song <ted.song@daumkakao.com>
2018-06-20 22:49:03 +0900

    RestController

--- src/main/java/com/github/uuidcode/querydsl/test/controller/UserController.java
+++ src/main/java/com/github/uuidcode/querydsl/test/controller/UserController.java
@@ -3,22 +3,20 @@ package com.github.uuidcode.querydsl.test.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.github.uuidcode.querydsl.test.entity.Payload;
 import com.github.uuidcode.querydsl.test.entity.User;
 import com.github.uuidcode.querydsl.test.service.UserService;
 import com.querydsl.core.types.Predicate;
 
-@Controller
+@RestController
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/user")
-    @ResponseBody
+    @GetMapping("/user")
     public Payload list(@QuerydslPredicate(root = User.class) Predicate predicate, Pageable pageable) {
         return this.userService.findAllWithJoin(predicate, pageable);
     }
