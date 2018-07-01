659dca22b8db38e22d7da307eee4156e251343b2
ted.song <ted.song@daumkakao.com>
2018-07-01 21:24:35 +0900

    findByAuthorId

--- src/main/java/com/github/uuidcode/querydsl/test/resolver/AuthorQueryResolver.java
+++ src/main/java/com/github/uuidcode/querydsl/test/resolver/AuthorQueryResolver.java
@@ -7,14 +7,24 @@ import org.springframework.stereotype.Component;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.github.uuidcode.querydsl.test.entity.Author;
+import com.github.uuidcode.querydsl.test.entity.Post;
 import com.github.uuidcode.querydsl.test.service.AuthorService;
+import com.github.uuidcode.querydsl.test.service.PostService;
 
 @Component
 public class AuthorQueryResolver implements GraphQLQueryResolver {
     @Autowired
     private AuthorService authorService;
 
+    @Autowired
+    private PostService postService;
+
     public List<Author> findAllAuthor() {
-        return this.authorService.findAll();
+        List<Author> authorList = this.authorService.findAll();
+        authorList.forEach(author -> {
+            List<Post> postList = this.postService.findByAuthorId(author.getId());
+            author.setPostList(postList);
+        });
+        return authorList;
     }
 }
--- src/main/java/com/github/uuidcode/querydsl/test/service/PostService.java
+++ src/main/java/com/github/uuidcode/querydsl/test/service/PostService.java
@@ -1,16 +1,23 @@
 package com.github.uuidcode.querydsl.test.service;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.github.uuidcode.querydsl.test.entity.Post;
+import com.github.uuidcode.querydsl.test.entity.QPost;
 
 @Service
 public class PostService extends QuerydslService<Post, Long> {
     @Autowired
     public PostService(EntityManager entityManager) {
         super(Post.class, entityManager);
+    }
+
+    public List<Post> findByAuthorId(Long authorId) {
+        return this.findAll(QPost.post.authorId.eq(authorId));
     }
 }
